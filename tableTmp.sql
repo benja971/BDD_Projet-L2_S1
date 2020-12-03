d61fe7029b7dc8683f1186d810214a480653fa6d
ghost-971 <57110591+ghost-971@users.noreply.github.com>
2020-12-02 22:46:57 -0400

    sqdq

--- tableTmp.sql
+++ tableTmp.sql
@@ -3,17 +3,40 @@
 -- WHERE code_type = 4
 -- ;
 
-SELECT matriculauteur, persnom, persprenom, equipnum, publications.projnom, nomrevue, titre, numordre
-FROM publications, membres, auteurs, revue
-WHERE code_type = 2
+SELECT matriculauteur, persnom, persprenom, membres.equipnum, publications.projnom, nomrevue, titre, numordre
+FROM publications, membres, auteurs, revue, projet
+WHERE publications.projnom = projet.projnom
+AND projet.persnum = membres.persnum
+AND code_type = 2
 ORDER BY persnom, persprenom;
 
--- ALTER TABLE publications ADD matriculauteur INT;
+-- ALTER TABLE auteurs ADD matriculauteur INT;
 
 
 
--- UPDATE publications 
+-- UPDATE auteurs 
 -- SET matriculauteur = 45
--- WHERE publinum = 30;
+-- WHERE persnum = 82;
 
--- SELECT* FROM publications;
+-- SELECT* FROM auteurs;
+
+
+
+-- 30	PUBLI_30	06/10/2002	projab	45	1
+-- 30	PUBLI_30	06/10/2002	projab	36	2
+-- 30	PUBLI_30	06/10/2002	projab	23	3
+-- 40	PUBLI_40	20/06/2003	projkk	28	1
+-- 10	PUBLI_10	15/08/2003	projxz	77	1
+-- 10	PUBLI_10	15/08/2003	projxz	87	2
+-- 20	PUBLI_20	30/05/2004	projxx	43	1
+
+-- 5	PUBLI_05	15/08/1999	Revue	2	projxx
+-- 10	PUBLI_10	15/08/2003	Interne	4	projxz
+-- 15	PUBLI_15	30/05/2001	Journal	3	projxz
+-- 20	PUBLI_20	30/05/2004	Interne	4	projxx
+-- 25	PUBLI_25	06/10/1999	Conference	1	projab
+-- 30	PUBLI_30	06/10/2002	Interne	4	projab
+-- 35	PUBLI_35	20/06/2001	Conference	1	projmo
+-- 40	PUBLI_40	20/06/2003	Interne	4	projkk
+-- 45	PUBLI_45	08/11/2000	Journal	3	projkl
+-- 55	PUBLI_55	15/05/2000	Conference	1	projxz
