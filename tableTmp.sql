409b1a1e4657ec749a9ba89fdb62796e75e83a7b
ghost-971 <57110591+ghost-971@users.noreply.github.com>
2020-12-02 22:21:46 -0400

    qds

--- tableTmp.sql
+++ tableTmp.sql
@@ -1,3 +1,19 @@
-SELECT matauteur_, persnom, persprenom, persnum, projnom, nomrevue, titreinterne, ordreauteurinterne
-FROM  membres, publiinterne, revue
-;
+-- SELECT publications.projnom, equipnum, publinum, titre
+-- FROM publications, equipes
+-- WHERE code_type = 4
+-- ;
+
+SELECT matriculauteur, persnom, persprenom, equipnum, publications.projnom, nomrevue, titre, numordre
+FROM publications, membres, auteurs, revue
+WHERE code_type = 2
+ORDER BY persnom, persprenom;
+
+-- ALTER TABLE publications ADD matriculauteur INT;
+
+
+
+-- UPDATE publications 
+-- SET matriculauteur = 45
+-- WHERE publinum = 30;
+
+-- SELECT* FROM publications;
